13ec3cda2c8d1a1cfd151f062f17d45fd597f73a
test01 - Ngoc Han <hanvan1331@gmail.com>
2026-03-14 20:12:31 +0700

    feat: add solution for challenge 03

--- tests/lesson-04/cloud-kingdom.js
+++ tests/lesson-04/cloud-kingdom.js
@@ -8,5 +8,5 @@ const level3 = 45;
 const sumCoin = level1 + level2 + level3;
 const avg = sumCoin / 3;
 
-const remainderCoin = sumCoin % 3 
-console.log("So coin du: " + remainderCoin)
+const remainderCoin = sumCoin % 3;
+console.log("So coin du: " + remainderCoin);
