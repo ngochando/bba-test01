cf495cd3b4cff26ee22fdb99f1462e81ebb0a030
test01 - Ngoc Han <hanvan1331@gmail.com>
2026-03-14 19:31:26 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,12 @@
+const playerName = "Mario";
+const currentLives = 3;
+
+const level1 = 25;
+const level2 = 30;
+const level3 = 45;
+
+const sumCoin = level1 + level2 + level3;
+const avg = sumCoin / 3;
+
+const remainderCoin = sumCoin % 3 
+console.log("So coin du: " + remainderCoin)
